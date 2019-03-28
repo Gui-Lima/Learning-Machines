0c8cfb2b664d2bbb2ebe42766a7f7e2cd21e8717
Guilherme Lima <guilherme.lima@AvilaSolucoes.intranet>
2019-03-28 15:21:42 -0300

    Refactor to kfold turning into a class

--- Separating Data/kFoldCrossValidation/kCrossValidation.py
+++ Separating Data/kFoldCrossValidation/kCrossValidation.py
@@ -1,125 +1,137 @@
 import random
 from os import path
 
-def writeFoldsInFile(folds):
-    f = open(path.abspath("Separating Data/kFoldCrossValidation/Folds.txt"), "w")
-    f.write("Fold Sizes: " + str(len(folds[0])))
-    f.write('\n')
-    f.write('\n')
-    for i in range(len(folds)):
-        f.write("------------------------------------")
-        f.write("Fold " + str(i+1) + " : ")
+class kFoldCrossValidation:
+
+    def __init__(self, dataset, k, validationColumn, debug=True):
+        self.dataset = dataset
+        self.k = k
+        self.debug = debug
+        self.validationColumn = validationColumn
+        self.groups = dataSet[validationColumn].tolist()
+
+    def writeFoldsInFile(self, folds):
+        f = open(path.abspath("Separating Data/kFoldCrossValidation/Folds.txt"), "w")
+        f.write("Fold Sizes: " + str(len(folds[0])))
         f.write('\n')
-        for j in range(len(folds[i])):
-            f.write(str(folds[i][j]))
+        f.write('\n')
+        for i in range(len(folds)):
+            f.write("------------------------------------")
+            f.write("Fold " + str(i+1) + " : ")
             f.write('\n')
-        f.write('\n')
-    f.close()
+            for j in range(len(folds[i])):
+                f.write(str(folds[i][j]))
+                f.write('\n')
+            f.write('\n')
+        f.close()
 
-def writeSetsInFile(trainingSet, AvaliationSet):
-    f = open(path.abspath("Separating Data/kFoldCrossValidation/TrainingAndAvaliationSets.txt"), "w")
-    f.write("Training sets size : " + str(len(trainingSet[0])))
-    f.write('\n')
-    f.write("Avaliation sets size : " + str(len(AvaliationSet[0])))
-    f.write('\n')
-    f.write('\n')
-    for i in range(len(trainingSet)):
-        f.write("In the " + str(i) + " iteration of the kfold, the training set will be : ")
+    def writeSetsInFile(self, trainingSet, AvaliationSet):
+        f = open(path.abspath("Separating Data/kFoldCrossValidation/TrainingAndAvaliationSets.txt"), "w")
+        f.write("Training sets size : " + str(len(trainingSet[0])))
         f.write('\n')
-        for j in range(len(trainingSet[i])):
-            f.write(str(trainingSet[i][j]) + " ,")
-
-        f.write('\n')
-        f.write(" And the avaliation set will be: ")
-        f.write('\n')
-
-        for j in range(len(AvaliationSet[i])):
-            f.write(str(AvaliationSet[i][j]) + " ,")
-
+        f.write("Avaliation sets size : " + str(len(AvaliationSet[0])))
         f.write('\n')
         f.write('\n')
-    f.close()
+        for i in range(len(trainingSet)):
+            f.write("In the " + str(i) + " iteration of the kfold, the training set will be : ")
+            f.write('\n')
+            for j in range(len(trainingSet[i])):
+                f.write(str(trainingSet[i][j]) + " ,")
+
+            f.write('\n')
+            f.write(" And the avaliation set will be: ")
+            f.write('\n')
+
+            for j in range(len(AvaliationSet[i])):
+                f.write(str(AvaliationSet[i][j]) + " ,")
+
+            f.write('\n')
+            f.write('\n')
+        f.close()
 
 
 
-def getPercentagesOfEachElement(l):
-    percentages = {}
-    elements = list(set(l))
-    for i in range(len(elements)):
-        element = elements[i]
-        percentage = l.count(element) / len(l)
-        percentages[element] = percentage
-        print("This group: " + str(element) + " represents " + str(percentage) + " of the database")
-    return percentages
+    def getPercentagesOfEachElement(self, l):
+        percentages = {}
+        elements = list(set(l))
+        for i in range(len(elements)):
+            element = elements[i]
+            percentage = l.count(element) / len(l)
+            percentages[element] = percentage
+            print("This group: " + str(element) + " represents " + str(percentage) + " of the database")
+        return percentages
 
-def kCrossValidation(k, groups, traningSets=[], avaliationSets=[], stratified=True):
-    print("-------------------------------")
-    print("Making Cross Validation")
-    datasetSize = len(groups)
-    setSize  = round(datasetSize/k)
-    print("The dataset size is " + str(datasetSize))
-    print("Since its " + str(k) + " folds, each fold will have " + str(setSize) + " elements, selected stratified = " + str(stratified))
-    if stratified:
-        folds = makeRandomFolds(k, groups)
-    else:
-        folds = makeFolds(groups)
+    def run(self, traningSets=[], avaliationSets=[], stratified=True):
+        print("-------------------------------")
+        print("Making Cross Validation")
+        datasetSize = len(self.groups)
+        setSize  = round(datasetSize/self.k)
+        print("The dataset size is " + str(datasetSize))
+        print("Since its " + str(self.k) + " folds, each fold will have " + str(setSize) + " elements, selected stratified = " + str(stratified))
+        if stratified:
+            folds = makeRandomFolds(self.k, self.groups)
+        else:
+            folds = makeFolds(self.groups)
 
-    for i in range(k):
-        trainingfold = []
-        selectedOutFold = i
-        for j in range(len(folds)):
-            if j is not i:
-                trainingfold.extend(folds[j])
-        traningSets.append(trainingfold)
-        avaliationSets.append(folds[i])
+        for i in range(k):
+            trainingfold = []
+            selectedOutFold = i
+            for j in range(len(folds)):
+                if j is not i:
+                    trainingfold.extend(folds[j])
+            traningSets.append(trainingfold)
+            avaliationSets.append(folds[i])
 
-    writeSetsInFile(traningSets, avaliationSets)
-    print("-------------------------------")
+        if self.debug:
+            writeSetsInFile(traningSets, avaliationSets)
+        print("-------------------------------")
 
 
-def makeRandomFolds(k, groups, seed=42):
-    folds = []
-    setSize  = round(len(groups)/k)
-    percentagesOfEachGroup = getPercentagesOfEachElement(groups)
-    groupAndIndex=[]
-    for i in range(len(groups)):
-        groupAndIndex.append((groups[i], i))
-    eachGroup = []
-    groupSets = list(set(groups))
-    ngroups = len(groupSets)
-    groupAndNumberOfElementsInEachFold = {}
-    for i in range(ngroups):
-        group = groupSets[i]
-        eachGroup.append([x for x in groupAndIndex if x[0] == group])
-        percentageOfThisGroupInTheDataset = percentagesOfEachGroup[group]
-        numberOfElementsOfThisGroup = setSize * percentageOfThisGroupInTheDataset
-        groupAndNumberOfElementsInEachFold[group] = round(numberOfElementsOfThisGroup)
-        print("Since each fold size is " + str(setSize) + " and the group " + str(group) +  " representation in the dataset is " + str(percentageOfThisGroupInTheDataset) + ", this group will have " + str(numberOfElementsOfThisGroup) + " elements")    
-    
-    for i in range(k):
-        fold = []
-        print("Making fold " + str(i + 1))
-        for j in range(ngroups):
-            print("Elements of this fold on group " + str(groupSets[j]) + " : " + str(groupAndNumberOfElementsInEachFold[groupSets[j]]))
-            fold.extend(getRandomElements(eachGroup[j], groupAndNumberOfElementsInEachFold[groupSets[j]]))
-        folds.append(fold)
+    def makeRandomFolds(self, seed=42):
+        folds = []
+        setSize  = round(len(self.groups)/self.k)
+        percentagesOfEachGroup = getPercentagesOfEachElement(self.groups)
+        groupAndIndex=[]
+        for i in range(len(self.groups)):
+            groupAndIndex.append((self.groups[i], i))
+        eachGroup = []
+        groupSets = list(set(self.groups))
+        ngroups = len(groupSets)
+        groupAndNumberOfElementsInEachFold = {}
+        for i in range(ngroups):
+            group = groupSets[i]
+            eachGroup.append([x for x in groupAndIndex if x[0] == group])
+            percentageOfThisGroupInTheDataset = percentagesOfEachGroup[group]
+            numberOfElementsOfThisGroup = setSize * percentageOfThisGroupInTheDataset
+            groupAndNumberOfElementsInEachFold[group] = round(numberOfElementsOfThisGroup)
+            print("Since each fold size is " + str(setSize) + " and the group " + str(group) +  " representation in the dataset is " + str(percentageOfThisGroupInTheDataset) + ", this group will have " + str(numberOfElementsOfThisGroup) + " elements")    
+        
+        for i in range(self.k):
+            fold = []
+            print("Making fold " + str(i + 1))
+            for j in range(ngroups):
+                print("Elements of this fold on group " + str(groupSets[j]) + " : " + str(groupAndNumberOfElementsInEachFold[groupSets[j]]))
+                fold.extend(getRandomElements(eachGroup[j], groupAndNumberOfElementsInEachFold[groupSets[j]]))
+            folds.append(fold)
 
-    for i in range(len(eachGroup)):
-        if(len(eachGroup[i]) > 0):
-            for j in range(len(eachGroup[i])):
-                folds[len(folds) - 1].extend(eachGroup[i][j])
+        for i in range(len(eachGroup)):
+            if(len(eachGroup[i]) > 0):
+                for j in range(len(eachGroup[i])):
+                    folds[len(folds) - 1].extend(eachGroup[i][j])
 
-    writeFoldsInFile(folds)
-    return folds
+        if self.debug:
+            writeFoldsInFile(folds)
+        return folds
 
-def getRandomElements(l, n):
-    elements  = []
-    for i in range(n):
-        if(len(l) != 0):
-            choice = random.choice(l)
-            l.remove(choice)
-            elements.append(choice)
-    return elements
+    def getRandomElements(self, l, n):
+        elements  = []
+        for i in range(n):
+            if(len(l) != 0):
+                choice = random.choice(l)
+                l.remove(choice)
+                elements.append(choice)
+        return elements
 
-def makeFolds(groups):
-    return []
+    def makeFolds(self):
+        # TODO
+        return []
