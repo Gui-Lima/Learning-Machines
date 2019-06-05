f3ca58548512bb60a1f5d2eaf2b53fcc5c7c69d4
Gui-Lima <ggfl@cin.ufpe.br>
2019-06-05 00:00:52 -0300

    new data

---
+++ DataSets/FinlandJoensuuJain/ReadingIt.py
@@ -0,0 +1,6 @@
+from os import path
+
+columns = ['x', 'y', 'label']
+
+relPath = path.abspath('DataSets/FinlandJoensuuJain/data.txt')
+
---
+++ DataSets/FinlandJoesuuFlame/ReadingIt.py
@@ -0,0 +1,6 @@
+from os import path
+
+columns = ['x', 'y', 'label']
+
+relPath = path.abspath('DataSets/FinlandJoesuuFlame/Data.txt')
+
